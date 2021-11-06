ba4cba110dba8dfa6dfd0d7281470a995a719edf
ktama <ktama@users.noreply.github.com>
2021-11-06 22:32:34 +0900

    add: data import

--- data_importer.py
+++ data_importer.py
@@ -1,3 +1,10 @@
+import pandas as pd
+
+
+def read_battle_result():
+    df = pd.read_csv('battle-results/2021-11-05.csv', header=1)
+    print(df)
+
 
 if __name__ == '__main__':
-    print('main func')
+    read_battle_result()
