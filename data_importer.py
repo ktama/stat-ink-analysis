4ec0bce77c2138cd3a26b33f0b8ba9df28144c77
ktama <ktama@users.noreply.github.com>
2021-11-07 00:01:21 +0900

    最新バージョンのXガチマッチのみ抽出

--- data_importer.py
+++ data_importer.py
@@ -1,9 +1,21 @@
 import pandas as pd
+import glob
 
 
 def read_battle_result():
-    df = pd.read_csv('battle-results/2021-11-05.csv', header=1)
-    print(df)
+    csv_files = glob.glob('battle-results/2021*.csv')
+
+    df = pd.DataFrame(columns=[])
+
+    for csv_file in csv_files:
+        tmp = pd.read_csv(csv_file)
+        df = pd.concat([df, tmp])
+
+    ver = df['game-ver'].max()
+
+    gachi = df.query(
+        '`lobby-mode` == "gachi" & lobby == "standard" & `A1-rank` == "x" & `game-ver` == @ver')
+    print(gachi)
 
 
 if __name__ == '__main__':
